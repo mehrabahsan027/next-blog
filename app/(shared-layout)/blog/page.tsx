2415d819575cb1414cc13bf601695ec185912146
mehrabahsan027 <mehrabahsan027@gmail.com>
2026-01-01 16:44:24 +0600

    cache

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -38,9 +38,9 @@ export default function BlogPage() {
 }
 
 async function LoadBlogList() {
-  // "use cache";
+  "use cache";
 //   cacheLife("hours");
-  // cacheTag("blog");
+  cacheTag("blog");
 
   await connection()
   const data = await fetchQuery(api.posts.getPosts);
